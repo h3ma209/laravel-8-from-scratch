1a80fc2f31007989954ff0b73969d1ebb9e62e84
hema omer <32086055+big-yoshi@users.noreply.github.com>
2021-07-17 16:21:43 +0300

    ep49

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -19,6 +19,7 @@ class RegisterController extends Controller
             "password"=>"required"
         ]);
         User::create($attributes);
+        session()->flash('success', "Your account has been created");
         return redirect("/");
     }
 }
